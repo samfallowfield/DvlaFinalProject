e214ea161ae8623a845696cbbea71f9a598892ef
sam fallowfield <sam.fallowfield1@gmail.com>
2022-03-29 17:10:27 +0100

    added functionality to post

--- Models/Client.cs
+++ Models/Client.cs
@@ -4,10 +4,10 @@ namespace DvlaProject.Models
     {
         public long Id { get; set; }
         public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        public string? DrivingLicenseNumber { get; set; }
-        public string? PostCode{ get; set; }
-        public string? EmailAddress { get; set; }
+        // public string? LastName { get; set; }
+        // public string? DrivingLicenseNumber { get; set; }
+        // public string? PostCode{ get; set; }
+        // public string? EmailAddress { get; set; }
         
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,10 +9,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<ClientContext>(opt =>
     opt.UseInMemoryDatabase("ClientInformation"));
-//builder.Services.AddSwaggerGen(c =>
-//{
-//    c.SwaggerDoc("v1", new() { Title = "DvlaProject", Version = "v1" });
-//});
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "DvlaProject", Version = "v1" });
+});
 
 var app = builder.Build();
 
@@ -20,8 +20,8 @@ var app = builder.Build();
 if (builder.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
-    //app.UseSwagger();
-    //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DvlaProject v1"));
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DvlaProject v1"));
 }
 
 app.UseHttpsRedirection();
